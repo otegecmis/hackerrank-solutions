2d061540844bc023202ca34f151bc886dcd2a715
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-08-29 14:56:03 +0300

    feat(hackerrank): add solution for `python-print`

---
+++ hackerrank/domains/python/python-print.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-print
+
+if __name__ == '__main__':
+    n = int(input())
+    result = ""
+    
+    for i in range(1, n + 1):
+        result += str(i)
+    
+    print(result)  
