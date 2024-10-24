9d5d542bc7425a1535bf3a73c925e2759fdea2f7
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-10-24 15:44:10 +0300

    feat(algorithms): add solution for `simple-array-sum`

---
+++ domains/algorithms/simple-array-sum/main.swift
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/simple-array-sum
+
+func simpleArraySum(ar: [Int]) -> Int {
+    var sum = 0
+    
+    for v in ar {
+        sum += v
+    }
+    
+    return sum
+}
