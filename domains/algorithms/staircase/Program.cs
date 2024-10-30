1c29534f36e0a9cd2a9e3d4bc31a819a68479020
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-10-30 14:19:34 +0300

    feat(algorithms): add solution for `staircase`

---
+++ domains/algorithms/staircase/Program.cs
@@ -0,0 +1,33 @@
+﻿// https://www.hackerrank.com/challenges/staircase
+namespace staircase;
+
+class Program
+{
+    public static void Line(int empty, int ch)
+    {
+        string line = "";
+
+        for (int i = 0; i < empty; i++)
+        {
+            line += " ";
+        }
+
+        for (int i = 0; i < ch; i++)
+        {
+            line += "#";
+        }
+
+        System.Console.WriteLine(line);
+    }
+    public static void staircase(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Line(n - i, i);
+        }
+    }
+    static void Main(string[] args)
+    {
+        staircase(6);
+    }
+}
