db9ae387bcda5f2cb28ce10b40097570f2602f75
otegecmis <177798054+otegecmis@users.noreply.github.com>
2025-01-15 11:43:56 +0300

    feat(data-structures): add solution for `arrays-ds`

---
+++ data-structures/arrays-ds/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    URL: https://www.hackerrank.com/challenges/arrays-ds
+    Author: @otegecmis
+*/
+
+class Result
+{
+    public static List<int> reverseArray(List<int> a)
+    {
+        List<int> myReverseList = new List<int>();
+
+        for (int i = a.Count - 1; i >= 0 ; i--)
+        {
+            myReverseList.Add(a[i]);
+        }
+
+        return myReverseList;
+    }
+}
