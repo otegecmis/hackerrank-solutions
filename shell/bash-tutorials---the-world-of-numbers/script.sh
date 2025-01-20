45ea60212cc2e91154cc1eef6a362df16cde75f6
otegecmis <177798054+otegecmis@users.noreply.github.com>
2025-01-20 15:36:14 +0300

    feat(shell): add solution for `bash-tutorials---the-world-of-numbers`

---
+++ shell/bash-tutorials---the-world-of-numbers/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# URL: https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers
+# Author: @otegecmis
+
+read x
+read y
+
+echo "$((x + y))"
+echo "$((x - y))"
+echo "$((x * y))"
+echo "$((x / y))"
