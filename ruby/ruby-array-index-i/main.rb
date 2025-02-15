6132a090ee54f4720f08414e95dd5bd078a41f1e
otegecmis <177798054+otegecmis@users.noreply.github.com>
2025-02-16 00:35:41 +0300

    feat(ruby): add solution for `ruby-array-index-i`

---
+++ ruby/ruby-array-index-i/main.rb
@@ -0,0 +1,18 @@
+# URL: https://www.hackerrank.com/challenges/ruby-array-index-i
+# Author: @otegecmis
+
+def element_at(arr, index)
+    return arr[index]
+end
+
+def inclusive_range(arr, start_pos, end_pos)
+    return arr[start_pos..end_pos]
+end
+
+def non_inclusive_range(arr, start_pos, end_pos)
+    return arr[start_pos...end_pos]
+end
+
+def start_and_length(arr, start_pos, length)
+    return arr[start_pos, length]
+end
