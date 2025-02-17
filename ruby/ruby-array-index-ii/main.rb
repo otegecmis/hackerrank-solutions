7ab9e7f833bdf1412f063dd5fb2d70cf255883ca
otegecmis <177798054+otegecmis@users.noreply.github.com>
2025-02-17 19:14:18 +0300

    feat(ruby): add solution for `ruby-array-index-ii`

---
+++ ruby/ruby-array-index-ii/main.rb
@@ -0,0 +1,22 @@
+# URL: https://www.hackerrank.com/challenges/ruby-array-index-ii
+# Author: @otegecmis
+
+def neg_pos(arr, index)
+    return arr[-index]
+end
+
+def first_element(arr)
+    return arr.first
+end
+
+def last_element(arr)
+    return arr.last
+end
+
+def first_n(arr, n)
+    return arr.take(n)
+end
+
+def drop_n(arr, n)
+    return arr.drop(n)
+end
